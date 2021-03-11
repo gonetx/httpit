587e76622eac6813725e8af35453e008929db6d5
Kiyon <kiyonlin@163.com>
2021-03-11 15:28:18 +0800

    feat(httpit): Add more flags

--- pit/config.go
+++ pit/config.go
@@ -5,22 +5,31 @@ import (
 )
 
 type Config struct {
-	Connections int
-	Count       int
-	Duration    time.Duration
-	Timeout     time.Duration
-
-	Method            string
-	Headers           []string
-	Host              string
-	Body              string
-	File              string
-	Cert              string
-	Key               string
-	HttpProxy         string
-	SocksProxy        string
-	Stream            bool
-	DisableKeepAlives bool
-	Insecure          bool
-	Pipeline          bool
+	Connections        int
+	Count              int
+	Duration           time.Duration
+	Timeout            time.Duration
+	Method             string
+	Headers            []string
+	Host               string
+	DisableKeepAlives  bool
+	Body               string
+	File               string
+	Stream             bool
+	JSON               bool
+	Form               bool
+	MultipartForm      bool
+	MultipartFormFiles []string
+	Boundary           string
+	Insecure           bool
+	Cert               string
+	Key                string
+	HttpProxy          string
+	SocksProxy         string
+	Pipeline           bool
+	Follow             bool
+	MaxRedirects       int
+	Output             string
+	Quite              bool
+	Debug              bool
 }
